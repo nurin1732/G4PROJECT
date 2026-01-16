0f0b43d7a2af1181d68a86af9e7e4ba02e33b842
Nurin Humaira Fauzi <24001732@siswa.um.edu.my>
2026-01-16 22:13:15 +0800

    Save local changes to StockCount

---
+++ src/units/StockCount.java
@@ -0,0 +1,26 @@
+package models;
+
+public class StockCount {
+    private String date;
+    private String time;
+    private String type;
+    private String model;
+    private int counted;
+    private int recorded;
+    private String employeeId;
+
+    public StockCount(String d, String t, String ty, String m, int c, int r, String e) {
+        date = d;
+        time = t;
+        type = ty;
+        model = m;
+        counted = c;
+        recorded = r;
+        employeeId = e;
+    }
+
+    public String toCSV() {
+        return date + "," + time + "," + type + "," + model + "," +
+               counted + "," + recorded + "," + employeeId;
+    }
+}
