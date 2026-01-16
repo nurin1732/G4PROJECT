f09e444b13cb65fef29fc61af0c38d3ce0611cc7
Nurin Humaira Fauzi <24001732@siswa.um.edu.my>
2026-01-16 20:02:58 +0800

    corrected

---
+++ src/units/Model.java
@@ -0,0 +1,29 @@
+package units;
+
+public class Model {
+    private String name;
+    private double price;
+    private int[] outletStock; // C60–C69
+
+    public Model(String name, double price, int[] outletStock) {
+        this.name = name;
+        this.price = price;
+        this.outletStock = outletStock;
+    }
+    
+    public void setStockAt(int index, int value) {
+        outletStock[index] = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStockAt(int index) {
+        return outletStock[index];
+    }
+}
