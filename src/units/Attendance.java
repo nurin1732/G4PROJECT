3b569036e082437075831f7d3a94ff2241b73471
Nurin Humaira Fauzi <24001732@siswa.um.edu.my>
2026-01-16 19:10:26 +0800

    corrected

---
+++ src/units/Attendance.java
@@ -0,0 +1,29 @@
+package units;
+
+public class Attendance {
+
+    private String employeeId;
+    private String employeeName;
+    private String date;
+    private String clockIn;
+    private String clockOut;
+
+    public Attendance(String employeeId, String employeeName,
+                      String date, String clockIn, String clockOut) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.date = date;
+        this.clockIn = clockIn;
+        this.clockOut = clockOut;
+    }
+
+    public String getEmployeeId() { return employeeId; }
+    public String getEmployeeName() { return employeeName; }
+    public String getDate() { return date; }
+    public String getClockIn() { return clockIn; }
+    public String getClockOut() { return clockOut; }
+
+    public void setClockOut(String clockOut) {
+        this.clockOut = clockOut;
+    }
+}
